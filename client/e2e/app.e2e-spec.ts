c419b376ddc76bc265191e103231f7d8be225fcb
Sean Spradlin <seanspradlin@gmail.com>
2018-01-10 14:58:21 -0600

    first attempt at tests

--- client/e2e/app.e2e-spec.ts
+++ client/e2e/app.e2e-spec.ts
@@ -7,6 +7,19 @@ describe('client App', () => {
     page = new AppPage();
   });
 
+  it('should load a list of tasks', async () => {
+    page.navigateTo();
+    const elements = await page.getToDoListElements();
+    expect(elements.length).toBeGreaterThan(0);
+  });
+
+  it('should have a delete button', async () => {
+    page.navigateTo();
+    const elements = await page.getToDoListElements();
+    const button = page.getDeleteButton(elements[0].id);
+    expect(button).toBeDefined();
+  });
+
   it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to app!');
--- client/e2e/app.po.ts
+++ client/e2e/app.po.ts
@@ -1,11 +1,32 @@
 import { browser, by, element } from 'protractor';
+import { Task } from '../src/app/models';
 
 export class AppPage {
-  navigateTo() {
+  async getToDoListElements(): Promise<Task[]> {
+    return element(by.css('#todolist'));
+  }
+
+  async clickCreateButton(): Promise<void> {
+    return element(by.css('#create')).click();
+  }
+
+  async clickDeleteButton(id: string): Promise<void> {
+    return element(by.css(`#del-${id}`)).click();
+  }
+
+  async navigateTo(): Promise<void> {
     return browser.get('/');
   }
 
-  getParagraphText() {
+  async getParagraphText(): Promise<string> {
     return element(by.css('app-root h1')).getText();
+  }
+
+  async clickFilterCompleted(): Promise<void> {
+    return element(by.css('#complete-filter')).click();
+  }
+
+  async clickFilterIncomplete(): Promise<void> {
+    return element(by.css('#incomplete-filter')).click();
   }
 }
